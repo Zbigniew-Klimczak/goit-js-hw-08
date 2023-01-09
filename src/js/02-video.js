91d68516a0a17997cd9ec8607483e6016317ae35
Zbigniew-Klimczak <111083175+Zbigniew-Klimczak@users.noreply.github.com>
2023-01-09 14:05:07 +0100

    Task 2 completed

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
-
+import Player from '@vimeo/player';
+import _ from 'lodash';
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+player.on(
+  'timeupdate',
+  _.throttle(data => {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+  }, 1000)
+);
+const lastTime = localStorage.getItem('videoplayer-current-time');
+player.setCurrentTime(lastTime);
